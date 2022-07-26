82039830d2d6984ac7fda41161e2e275dd1856af
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-07-26 10:22:36 -0500

    psuh for deployment

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
-// import { Button } from 'react-bootstrap';
 import { getPlayers } from '../api/teamData';
 import PlayerCard from '../components/PlayerCard';
-// import { signOut } from '../utils/auth';
+import Search from '../components/Search';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const [players, setPlayers] = useState([]);
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
   const { user } = useAuth();
   const getAllPlayers = () => {
-    getPlayers(user.uid).then(setPlayers);
+    getPlayers(user.uid).then((playerArr) => {
+      setPlayers(playerArr);
+      setFilteredPlayers(playerArr);
+    });
   };
   useEffect(() => {
     getAllPlayers();
-  }, []);
+  }, [user]);
 
   return (
-
-    <div className="d-flex flex-wrap">
-      {players.map((player) => (
-        <PlayerCard key={player.firebaseKey} playerObj={player} onUpdate={getAllPlayers} />
-      ))}
-    </div>
-
+    <>
+      <Search players={players} setFilteredPlayers={setFilteredPlayers} />
+      <div className="d-flex flex-wrap">
+        {filteredPlayers.map((player) => (
+          <PlayerCard key={player.firebaseKey} playerObj={player} onUpdate={getAllPlayers} />
+        ))}
+      </div>
+    </>
   );
 }
 
